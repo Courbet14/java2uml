1ee143f893995d9b9b3c367fb317a41e7b92c7db
photo <68138422+Courbet14@users.noreply.github.com>
2025-04-16 23:09:30 +0900

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,9 +1,21 @@
-#make dhirectory
+#!/bin/bash
+
+# 保存元の作業ディレクトリ（key.jsonのある場所）
+ORIGIN_DIR="$(pwd)"
+
+# ディレクトリ作成
 [ -d "$HOME/.tools/java2uml" ] || mkdir -p "$HOME/.tools/java2uml"
-cd ~/.tools/
+
+# .toolsへ移動
+cd "$HOME/.tools/"
 rm -fr java2uml
-#download files
-git clone https://github.com/Courbet14/java2uml.git > /dev/null #2>&1
+
+# リポジトリをクローン
+git clone https://github.com/Courbet14/java2uml.git > /dev/null
+
+# key.json をコピー（元のディレクトリから）
+cp "$ORIGIN_DIR/key.json" "$HOME/.tools/java2uml/key.json"
+# セットアップ実行
 cd java2uml
 chmod +x setup.sh
 ./setup.sh
